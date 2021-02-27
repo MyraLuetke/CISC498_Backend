7849aee7f9a3ca957728f9db6ff55d5b5cb610e8
Myra Luetke <16mfl1@caslab.queensu.ca>
2021-02-27 15:46:04 -0500

    adds cors mechanism so frontend can communciate with backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'corsheaders',
     'checkin.apps.CheckinConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,7 +58,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True #change this to CORS_ORIGIN_WHITELIST = ('http://localhost:8080','http://127.0.0.1:9000')
 
 ROOT_URLCONF = 'backend.urls'
 
